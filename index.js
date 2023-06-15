5f9488aeca554d02e9789772a1cb13dfa947e0a8
motikev <kmotties@gmail.com>
2023-06-15 19:35:35 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-function addingEventListener() {
-}
+require('./helpers.js');
+
+const sinon = require('sinon');
+
+describe("index.js", () => {
+  let input;
+
+  beforeEach(function() {
+    // Create a fake button element
+    input = document.createElement('button');
+    input.id = 'button';
+    document.body.appendChild(input);
+
+    // Spy on the addEventListener method of the button element
+    sinon.spy(input, 'addEventListener');
+  });
+
+  afterEach(function() {
+    // Clean up the fake button element
+    document.body.removeChild(input);
+  });
+
+  it("binds an event listener in addingEventListener()", () => {
+    addingEventListener();
+    expect(input.addEventListener.called).to.be.true;
+  });
+});
